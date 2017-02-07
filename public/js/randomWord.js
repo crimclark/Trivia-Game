2598d69cac83cbec7c10fea385d369b64b1572b6
Tim Clark <oeomusic@gmail.com>
2017-02-07 13:56:27 -0800

    tracks randomWord.js

---
+++ public/js/randomWord.js
@@ -0,0 +1,18 @@
+var randWord = function(){
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  var randomWord = [];
+  var i = 0;
+  while(i < 8){
+    var randomNumber = Math.floor(Math.random() * 26);
+    var randomLetter = alphabet.charAt(randomNumber);
+    randomWord.push(randomLetter);
+    i++;
+  }
+  randomWord = randomWord.join('');
+  // console.log('I am short word: ' + randomWord);
+  return randomWord;
+}
+
+// console.log(randWord());
+
+

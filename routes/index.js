a1db83bce3586de85d3184a19cb60d8515800132
an nguyen <hello.an.ng@gmail.com>
2017-02-06 18:03:26 -0800

    sets up handlebars files

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const Handlebars = require('handlebars');
 
 router.get('/', (req, res, next) => {
-  res.render('index', {title: 'Home'})
+  res.render('index', {title: 'Login Page'});
+})
+
+router.get('/new', (req, res, next) => {
+  res.render('new', {title: 'New Game'});
+})
+
+router.get('/user', (req, res, next) => {
+  res.render('profile', {title: 'Player Profile'});
 })
 
 router.get('/test', (req, res, next) => {

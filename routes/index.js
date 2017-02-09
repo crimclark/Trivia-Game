d9e451392c3d3547e30ac5118820f7b05b9002ea
Justin Samuels <blizm87@gmail.com>
2017-02-09 13:06:15 -0800

    Increments activeUser count in db and retrieves a gameroom with only one activeUser

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Handlebars = require('handlebars');
 const getQuestion = require('../lib/getQuestion.js');
-const answerShuffle = require('../lib/answerShuffle.js')
+const answerShuffle = require('../lib/answerShuffle.js');
 const Profile = require('../models/userProfiles.js');
 const gameRooms = require('../models/gameRooms.js');
 
@@ -10,7 +10,7 @@ router.get('/', (req, res, next) => {
   if (!req.session.user) {
     res.render('index', {title: 'Login Page'});
   } else {
-    const user = JSON.stringify(req.session.user)
+    const user = JSON.stringify(req.session.user);
     res.render('new', {title: 'New Game'});
   }
 });
@@ -23,16 +23,16 @@ router.get('/question', (req, res, next) => {
   getQuestion(function(data, question) {
     answerShuffle.answerShuffle(data, function(shuffleData) {
       res.send({question: question, answers: shuffleData});
-    })
-  })
-})
+    });
+  });
+});
 
 router.get('/game/:id', (req, res, next) => {
   var fullUrl = '/game/' + req.params.id;
   // test /game/XIttXxHc
   gameRooms.find({url: fullUrl }, function(err, results) {
     if (err) {
-      console.log(err)
+      console.log(err);
     }
     if (results.length === 0) {
       getQuestion(function(data, question) {
@@ -47,24 +47,26 @@ router.get('/game/:id', (req, res, next) => {
           });
           gameRoom.save();
           res.render('game', {question: question, answers: shuffleData, gameUrl: fullUrl});
-          })
-        })
+          });
+        });
       }
       else if(results) {
         gameRooms.find({url: fullUrl}, function(err, results) {
           var formatted_results = results[0].firstQuestion[0];
-         res.render('game', {question: formatted_results.question, answers: formatted_results.answers})
-        })
+         res.render('game', {question: formatted_results.question, answers: formatted_results.answers});
+        });
       }
-    })
+    });
 });
 
 
 router.get('/join', (req, res, next) => {
-  gameRooms.find({}, (err, allRooms) => {
+  gameRooms.find({activeUsers: 1}, (err, allRooms) => {
     var rand = Math.floor(Math.random() * allRooms.length);
     var joinUrl = allRooms[rand].url;
-    res.redirect(joinUrl);
+    gameRooms.update({url: joinUrl}, { $inc: { activeUsers: 1 }},(err, userData) => {
+      res.redirect(joinUrl);
+    });
   });
 });
 
@@ -72,10 +74,10 @@ router.delete('/game/:id', (req, res, next) => {
   var fullUrl = '/game/' + req.params.id;
   gameRooms.findOneAndRemove({url: fullUrl}, function(err) {
     if (err) {
-      console.log(err)
+      console.log(err);
     }
-  })
-})
+  });
+});
 
 router.get('/user', (req, res, next) => {
   var userId = req.session.user.id;
@@ -86,9 +88,9 @@ router.get('/user', (req, res, next) => {
 
 router.get('/user.json', (req, res, next) => {
   Profile.find({}, 'name avatar score.gamesWon score.gamesPlayed', function(err, userData) {
-    res.send(userData)
-  })
-})
+    res.send(userData);
+  });
+});
 
 router.post('/user', (req, res, next) => {
   var userId = req.session.user.id;
@@ -111,7 +113,7 @@ router.get('/browse', (req, res, next) => {
 });
 
 router.get('/test', (req, res, next) => {
-  res.render('test')
+  res.render('test');
 });
 
 module.exports = router
